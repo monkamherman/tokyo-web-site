f658bc90143273111ffe2680092e0789f0ee6e9d
MONKAM HERMAN <cesaristos5@gmail.com>
2024-10-23 18:49:20 +0100

    section 1

--- front-end/src/page/component/home main/HomeMain.tsx
+++ front-end/src/page/component/home main/HomeMain.tsx
@@ -1,71 +1,73 @@
 import Stays from "./component/Stays"
 import Bouton from "@/components/ui/utile/Button"
 import { SetStateAction, useState } from "react";
+import Section_1 from "./section 1/Section-1";
 
 
-// const Stays = () => <div> jjj</div>;
 const Experiences = () => <div>Contenu pour Experiences</div>;
 const Cars = () => <div>Contenu pour Cars</div>;
 const Flights = () => <div>Contenu pour Flights</div>;
 
 
 function HomeMain() {
-    
-  const [activeButton, setActiveButton] = useState('Stays');
 
-  const handleButtonClick = (buttonName: SetStateAction<string>) => {
-    setActiveButton(buttonName);
-  };
+    const [activeButton, setActiveButton] = useState('Stays');
 
-  const getButtonClass = (buttonName: string) => {
-    return `text-[1.4rem] font-semibold ${
-      activeButton === buttonName ? 'text-black' : 'text-gray-500'
-    }`;
-  };
+    const handleButtonClick = (buttonName: SetStateAction<string>) => {
+        setActiveButton(buttonName);
+    };
 
-  const renderActiveComponent = () => {
-    switch (activeButton) {
-      case 'Stays':
-        return <Stays />;
-      case 'Experiences':
-        return <Experiences />;
-      case 'Cars':
-        return <Cars />;
-      case 'Flights':
-        return <Flights />;
-      default:
-        return null;
-    }
-  };
+    const getButtonClass = (buttonName: string) => {
+        return `text-[1.4rem] font-semibold ${activeButton === buttonName ? 'text-black' : 'text-gray-500'
+            }`;
+    };
+
+    const renderActiveComponent = () => {
+        switch (activeButton) {
+            case 'Stays':
+                return <Stays />;
+            case 'Experiences':
+                return <Experiences />;
+            case 'Cars':
+                return <Cars />;
+            case 'Flights':
+                return <Flights />;
+            default:
+                return null;
+        }
+    };
     return (
-        <div className='flex p-4 '>
-            <div className="flex-1  pl-8 flex flex-col gap-4">
-                <h1 className="text-[4rem] font-semibold">Hotel, car & experiences</h1>
-                <p className="text-[1rem] text-gray-600">Accompanying us, you ar a trip full of experiences. White chisfis, bookinf accommodation, resort villas, hotels</p>
-                <Bouton label="Start your search" />
-                <div className="relative flex justify-between pr-[30%] pl-6 pb-28 ">
-                    <button className={getButtonClass('Stays')} onClick={() => handleButtonClick('Stays')}>Stays</button>
-                    <button className={getButtonClass('Experiences')} onClick={() => handleButtonClick('Experiences')}>Experiances</button>
-                    <button className= {getButtonClass('Cars')} onClick={() => handleButtonClick('Cars')}>Cars</button>
-                    <button className= {getButtonClass('Flights')} onClick={() => handleButtonClick('Flights')}>Flights</button>
-                    <div className="absolute top-12 w-[70rem] py-8 px-6 rounded-full shadow-md bg-white">
-                    {renderActiveComponent()}
+        <>
+            <div className='flex p-4 '>
+                <div className="flex-1  pl-8 flex flex-col gap-4">
+                    <h1 className="text-[4rem] font-semibold">Hotel, car & experiences</h1>
+                    <p className="text-[1rem] text-gray-600">Accompanying us, you ar a trip full of experiences. White chisfis, bookinf accommodation, resort villas, hotels</p>
+                    <Bouton label="Start your search" />
+                    <div className="relative flex justify-between pr-[30%] pl-6 pb-28 ">
+                        <button className={getButtonClass('Stays')} onClick={() => handleButtonClick('Stays')}>Stays</button>
+                        <button className={getButtonClass('Experiences')} onClick={() => handleButtonClick('Experiences')}>Experiances</button>
+                        <button className={getButtonClass('Cars')} onClick={() => handleButtonClick('Cars')}>Cars</button>
+                        <button className={getButtonClass('Flights')} onClick={() => handleButtonClick('Flights')}>Flights</button>
+                        <div className="absolute top-12 w-[70rem] py-8 px-6 rounded-full shadow-md bg-white">
+                            {renderActiveComponent()}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex-1 grid grid-cols-2 grid-rows-2 gap-4   ">
-                <div className="bg-slate-400">
-                    <img src="/logo.svg" alt="" />
+                <div className="flex-1 grid grid-cols-2 grid-rows-2 gap-4   ">
+                    <div className="bg-slate-400">
+                        <img src="/logo.svg" alt="" />
+                    </div>
+                    <div className="row-span-2 bg-slate-600 mt-12">
+                        <img src="/logo.svg" alt="" />
+                    </div>
+                    <div className="bg-slate-500 ">
+                        <img src="/logo.svg" alt="" />
+                    </div>
                 </div>
-                <div className="row-span-2 bg-slate-600 mt-12">
-                    <img src="/logo.svg" alt="" />
-                </div>
-                <div className="bg-slate-500 ">
-                    <img src="/logo.svg" alt="" />
-                </div>
-            </div>
 
-        </div>
+            </div>
+            <Section_1/>
+        </>
     )
 }
 
--- front-end/src/page/component/home main/section 1/Section-1.tsx
+++ front-end/src/page/component/home main/section 1/Section-1.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 function Section_1() {
   return (
-    <div>
-      
+    <div className="mt-[6rem] ml-[3rem] ">
+      <h1 className="text-[2rem] font-bold">Suggestions for discovery</h1>
+      <p className="text-[1.2rem] text-gray-500 ">popular places to recommends for you</p>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={5}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className='h-[4rem]'>
+            <div className='bg-gray-500 h-[4rem]'>
+              <img src="/logo.svg" className='h-[3rem] w-2 rounded-lg' alt="" />
+            </div>
+            <h1>Londre</h1>
+            <p>288,888</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide><div>kk</div></SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        ...
+      </Swiper>
+
     </div>
   )
 }
